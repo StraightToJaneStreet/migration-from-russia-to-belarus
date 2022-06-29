c2949363754ffa9cebb60a4c31340515704648f3
Straight To <straighttojanestreet@rs.school>
2022-06-29 22:49:52 +0300

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '23eda7a9a1ba47178e932adfe3cfdb26', // получите свой ключ https://newsapi.org/
         });
     }
 }
