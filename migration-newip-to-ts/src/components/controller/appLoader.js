e8ec18822c593bd28604d331a1baf12dd7bc74a5
Straight To <straighttojanestreet@rs.school>
2022-06-29 23:29:15 +0300

    chore: change shiftwidth for all source files

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '23eda7a9a1ba47178e932adfe3cfdb26', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '23eda7a9a1ba47178e932adfe3cfdb26', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
