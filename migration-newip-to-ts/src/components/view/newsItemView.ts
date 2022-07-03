550f0714ee7fd6fbbec7c76a2e76c60b98116815
Straight To <straighttojanestreet@rs.school>
2022-07-03 21:41:59 +0300

    refactor: move article render logic from News to NewsItemView function

---
+++ migration-newip-to-ts/src/components/view/newsItemView.ts
@@ -0,0 +1,54 @@
+import NewsItem from '../core/NewsItem';
+
+function createElement(tag: string, classes: string[]): HTMLElement {
+  const element = document.createElement('tag');
+  element.classList.add(...classes);
+  return element;
+}
+
+export default function NewsItemView(article: NewsItem, alt: boolean) {
+  const newsItem = createElement('div', ['news__item']);
+  if (alt) {
+    newsItem.classList.add('alt');
+  }
+
+  const metaContainer = createElement('div', ['news__meta']);
+
+  const metaPhoto = createElement('div', ['news__meta-photo']);
+  metaPhoto.style.backgroundImage = article.urlToImage || 'img/news_placeholder.jpg';
+
+  const metaDetails = createElement('ul', ['news__meta-details']);
+  const metaAuthor = createElement('li', ['news__meta-author']);
+  metaAuthor.textContent = article.author || article.source.name;
+
+  const metaDate = createElement('li', ['news__meta-date']);
+  metaDate.textContent = article.publishedAt
+    .slice(0, 10)
+    .split('-')
+    .reverse()
+    .join('-');
+
+  const description = createElement('div', ['news__description']);
+  const title = createElement('h2', ['news__description-title']);
+  title.textContent = article.title;
+
+  const source = createElement('h3', ['news__description-source']);
+  source.textContent = article.source.name;
+
+  const content = createElement('p', ['news__description-content']);
+  content.textContent = article.description;
+
+  const readMore = createElement('p', ['news__read-more']);
+
+  const readMoreLink = document.createElement('a');
+  readMoreLink.href = '#';
+  readMoreLink.href = article.url;
+
+  newsItem.append(metaContainer, description);
+  metaContainer.append(metaPhoto, metaDetails);
+  metaDetails.append(metaAuthor, metaDate);
+  description.append(title, source, content, readMore);
+  readMore.append(readMoreLink);
+
+  return newsItem;
+}
--- migration-newip-to-ts/src/components/view/newsView.ts
+++ migration-newip-to-ts/src/components/view/newsView.ts
@@ -1,33 +1,19 @@
-import './news.css';
+import './css/newsView.css';
+import NewsItem from '../core/NewsItem';
+import NewsItemView from './newsItemView';
 
 class News {
-  draw(data) {
-    const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+  draw(news: NewsItem[]) {
+    // const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+    const lastNews = news.length >= 10 ? news.filter((_, idx) => idx < 10) : news;
 
     const fragment = document.createDocumentFragment();
-    const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp');
 
-    news.forEach((item, idx) => {
-      const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
+    news.forEach((article, idx) => {
+      const alternativeView = idx % 2 == 0;
+      const newsItemElement = NewsItemView(article, alternativeView);
 
-      if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
-      const photo = newsClone.querySelector('.news__meta-photo') as HTMLElement;
-      photo.style.backgroundImage = `url(${
-        item.urlToImage || 'img/news_placeholder.jpg'
-      })`;
-      newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-      newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
-        .slice(0, 10)
-        .split('-')
-        .reverse()
-        .join('-');
-
-      newsClone.querySelector('.news__description-title').textContent = item.title;
-      newsClone.querySelector('.news__description-source').textContent = item.source.name;
-      newsClone.querySelector('.news__description-content').textContent = item.description;
-      newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
-
-      fragment.append(newsClone);
+      fragment.append(newsItemElement);
     });
 
     document.querySelector('.news').innerHTML = '';
