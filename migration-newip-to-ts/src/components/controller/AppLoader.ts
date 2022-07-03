a9b4940bfb20ffff73ac97e83005329a1daccc97
Straight To <straighttojanestreet@rs.school>
2022-07-03 22:30:47 +0300

    fix: make endpoint as part of LoaderOptions object

--- migration-newip-to-ts/src/components/controller/AppLoader.ts
+++ migration-newip-to-ts/src/components/controller/AppLoader.ts
@@ -2,7 +2,8 @@ import Loader from './Loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super({
+      baseLink: 'https://newsapi.org/v2/',
       apiKey: '23eda7a9a1ba47178e932adfe3cfdb26', // получите свой ключ https://newsapi.org/
     });
   }
