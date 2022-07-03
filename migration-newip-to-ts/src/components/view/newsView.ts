86ad5faa406de88e2c613b81b3afa3a22581fcb6
Straight To <straighttojanestreet@rs.school>
2022-07-03 21:53:30 +0300

    feat: add root container for rendering News

--- migration-newip-to-ts/src/components/view/newsView.ts
+++ migration-newip-to-ts/src/components/view/newsView.ts
@@ -3,6 +3,14 @@ import Article from '../core/Article';
 import ArticleView from './ArticleView';
 
 class News {
+  container: HTMLDivElement;
+
+  constructor(root: HTMLElement) {
+    this.container = document.createElement('div');
+    this.container.classList.add('news');
+    root.append(this.container);
+  }
+
   draw(news: Article[]) {
     const lastNews = news.length >= 10 ? news.filter((_, idx) => idx < 10) : news;
 
@@ -15,8 +23,8 @@ class News {
       fragment.append(newsItemElement);
     });
 
-    document.querySelector('.news').innerHTML = '';
-    document.querySelector('.news').appendChild(fragment);
+    this.container.innerHTML = '';
+    this.container.append(fragment);
   }
 }
 
