0c0f270e31be930289daada4ac26a74228216e00
Straight To <straighttojanestreet@rs.school>
2022-07-03 22:03:15 +0300

    refactor: move HTMLTemplate rendering to SourceView

--- migration-newip-to-ts/src/components/view/sourceView.ts
+++ migration-newip-to-ts/src/components/view/sourceView.ts
@@ -1,11 +1,19 @@
 import Source from '../core/source';
 
-export default class SourceView {
-  constructor(source: Source) {
+function createElement(tag: string, classes: string[]): HTMLElement {
+  const element = document.createElement('tag');
+  element.classList.add(...classes);
+  return element;
+}
 
-  }
+export default function SourceView(source: Source) {
+  const item = createElement('div', ['source__item']);
+  item.setAttribute('data-source-id', source.id);
 
-  createElement() {
+  const name = createElement('span', ['source__item-name']);
+  name.textContent = source.name
 
-  }
+  item.append(name);
+
+  return item;
 }
