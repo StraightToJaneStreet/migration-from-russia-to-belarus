12be3414b2442d857aa336e06c59726e055c57bd
Straight To <straighttojanestreet@rs.school>
2022-07-04 21:01:21 +0300

    chore: fix typo

--- migration-newip-to-ts/src/components/controller/SourcesLoader.ts
+++ migration-newip-to-ts/src/components/controller/SourcesLoader.ts
@@ -3,7 +3,7 @@ import Loader from './Loader';
 import Source from '../core/Source';
 import SourcesResponse from '../app/SourcesResponse';
 
-export default class ArticlesLoader extends Loader<Source[], SourcesResponse> {
+export default class SourcesLoader extends Loader<Source[], SourcesResponse> {
   protected unwrapResponse(response: Response): Promise<Source[]> {
       const unwrapper = new SourcesResponse(response);
       return unwrapper.extractEntities();
